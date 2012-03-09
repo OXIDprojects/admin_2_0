8cbb30b5a819e74f1310f1d38b5956d062a82c10
Dave Holloway <dh@dajoho.de>
2012-03-09 22:00:17 +0100

    OXID Core

--- api/rest/index.php
+++ api/rest/index.php
@@ -1,14 +1,21 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+function getShopBasePath()
+{
+    return dirname(__FILE__).'/../../';
+}
+
+/**
+ * Load OXID Core Classes
+ */
+require getShopBasePath() . 'modules/functions.php';
+require_once getShopBasePath() . 'core/oxfunctions.php';
+$myConfig = oxConfig::getInstance();
+oxUtils::getInstance()->stripGpcMagicQuotes();
+$iDebug = $myConfig->getConfigParam('iDebug');
+set_exception_handler(array(oxNew('oxexceptionhandler', $iDebug), 'handleUncaughtException'));
 
 #require_once('lib/...');
 require_once('dispatcher.php');
-=======
-if ( !function_exists( 'isAdmin' )) {
-    function isAdmin()
-    {
-        return true;
-    }
-}
-
-// Includes main index.php file
-require_once dirname(__FILE__)."/../../index.php";
