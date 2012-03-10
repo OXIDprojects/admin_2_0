972b1095f88bb6b0ce63b94a64e6dfcbed226ae2
Dave Holloway <dh@dajoho.de>
2012-03-10 09:46:10 +0100

    Whoops. Moved folders to correct location.

--- api/rest/dispatcher.php
+++ api/rest/dispatcher.php
@@ -1,6 +1,31 @@
 <?php
-$url = explode('/rest/', $_SERVER['REQUEST_URI'],2);
-if (count($url)==2) {
-    echo $url[1];
+class admin2 {
+    static public function request($key)
+    {
+        return oxConfig::getParameter($key);
+    }
+
+    static public function main()
+    {
+    
+        $subject = $_SERVER['REQUEST_URI'];
+        $pattern = '/.*\/rest\/(?P<version>.*)\/(?P<class>.*)[\.](?P<format>.*)\?.*/';
+        preg_match($pattern, $subject, $matches,0);
+        
+        echo '<pre>';
+        print_r($matches);
+        
+        
+        /*$url = explode('/rest/', $_SERVER['REQUEST_URI'],2);
+        if (count($url)==2)
+        {
+            echo $url[1];
+        }
+        */
+        
+        echo self::request('fields');
+    
+    }
 }
+admin2::main();
 ?>
