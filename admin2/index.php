bbca0b7edaae7b803f1895910748de692a76cea8
Rafael Dabrowski <rafael.dabrowski@heiner-direct.com>
2012-03-11 18:34:03 +0100

    Added Charting Demo

--- admin2/index.php
+++ admin2/index.php
@@ -31,7 +31,8 @@ $t = new Admin_Template();
         <script src="js/modernizr.custom.js"></script>
         <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
         <script src="js/jquery.dataTables.min.js" type="text/javascript"></script>
-
+		<script src="js/raphael.js" type="text/javascript"></script>
+		<script src="js/amcharts.js" type="text/javascript"></script>
     </head>
     <body>
         <header id="oxhead">
