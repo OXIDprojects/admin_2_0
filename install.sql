2f1af38428975575892eb4d6a3916d158b966d43
tabsl <tobias@merkl.eu>
2012-03-15 17:11:13 +0100

    managing api access for shopuser / create authcode (api-key)

---
+++ install.sql
@@ -0,0 +1,11 @@
+--
+-- CREATE adm2rest_api
+--
+CREATE TABLE IF NOT EXISTS `adm2rest_api` (
+  `OXID` char(32) COLLATE latin1_general_ci NOT NULL,
+  `OXUSERID` char(32) COLLATE latin1_general_ci NOT NULL,
+  `OXAPIACCESS` tinyint(1) NOT NULL,
+  `OXAPIACCESSKEY` char(50) COLLATE latin1_general_ci NOT NULL,
+  `OXUPDATE` int(11) NOT NULL,
+  PRIMARY KEY (`OXID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
