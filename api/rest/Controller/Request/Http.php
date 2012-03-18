6f40f23cfc7c71fdb34b806d06ce6273218a7dc4
DSB <admin@mysqldumper.de>
2012-03-18 11:31:15 +0100

    QA

--- api/rest/Controller/Request/Http.php
+++ api/rest/Controller/Request/Http.php
@@ -12,34 +12,26 @@ class Admin2_Controller_Request_Http extends Admin2_Controller_Request_Abstract
             throw new Admin2_Controller_Request_Exception("Request URI is not set.");
         }
 
-        $baseDir = dirname($_SERVER['SCRIPT_NAME']) . '/';
+        $baseDir         = dirname($_SERVER['SCRIPT_NAME']) . '/';
         $cleanRequestUri = str_replace($baseDir, '', $_SERVER['REQUEST_URI']);
-        $urlParts = explode('/', trim($cleanRequestUri, '/'));
-        $count = count($urlParts);
-        $formatPos = strpos($urlParts[$count - 1], '.');
+        $urlParts        = explode('/', trim($cleanRequestUri, '/'));
+        $count           = count($urlParts);
+        $formatPos       = strpos($urlParts[$count - 1], '.');
 
         if ($formatPos !== false) {
-            $this->_format = substr($urlParts[$count - 1], $formatPos + 1);
+            $this->_format        = substr($urlParts[$count - 1], $formatPos + 1);
             $urlParts[$count - 1] = substr($urlParts[$count - 1], 0, $formatPos);
         }
 
-        if ($count > 0) {
-            $this->_version = substr($urlParts[0], 1);
-        }
+        $this->_version    = $count > 0 ? substr($urlParts[0], 1) : 1;
+        $this->_controller = $count > 1 ? $urlParts[1] : 'Products';
+        $this->_entity     = $count > 2 ? $urlParts[2] : '';
 
-        if ($count > 1) {
-            $this->_controller = $urlParts[1];
-        }
-
-        if ($count > 2) {
-            $this->_entity = $urlParts[2];
-        }
-
-        if (isset($_REQUEST) && !empty($_REQUEST)) {
+        if (!empty($_REQUEST)) {
             $this->_params = filter_var_array($_REQUEST);
         }
 
-        if (isset( $_SERVER['REQUEST_METHOD']) && !empty( $_SERVER['REQUEST_METHOD'])) {
+        if (!empty($_SERVER['REQUEST_METHOD'])) {
             $this->_method = $_SERVER['REQUEST_METHOD'];
         }
     }
