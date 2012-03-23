f0734900743cbc85fa725697dded1e7641ababd5
DSB <admin@mysqldumper.de>
2012-03-23 22:34:38 +0100

    Changed Output/Processor/Csv.php - added headline with names of fields - added escaping of values

--- api/rest/Output/Processor/Csv.php
+++ api/rest/Output/Processor/Csv.php
@@ -5,7 +5,15 @@ class Admin2_Output_Processor_Csv implements Admin2_Output_Processor_Interface
     public function process(Admin2_Controller_Result $result)
     {
         $result->addResponseHeader('Content-Type', 'text/plain', true);
-
-        return implode(',', $result->getData());
+        $data = $result->getData();
+        $keys = array_keys($data);
+        $key = $keys[0];
+        $fields = array_keys($data[$key]);
+        $headLine = implode(',', $fields);
+        $ret = $headLine ."\n";
+        foreach ($data as $line) {
+            $ret .= implode('","', str_replace('"', '\"', $line)) . "\n";
+        }
+        return $ret;
     }
 }
