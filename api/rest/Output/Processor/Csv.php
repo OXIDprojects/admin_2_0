6506e5ce5af134d2a9b67c9dd0b07c551bf2e11b
DSB <admin@mysqldumper.de>
2012-03-23 22:37:28 +0100

    Changed Admin2_Output/Processor/Csv.php: - added DocBlock

--- api/rest/Output/Processor/Csv.php
+++ api/rest/Output/Processor/Csv.php
@@ -2,6 +2,13 @@
 
 class Admin2_Output_Processor_Csv implements Admin2_Output_Processor_Interface
 {
+    /**
+     * Return result set as CSV
+     *
+     * @param Admin2_Controller_Result $result Result set as array
+     *
+     * @return string Converted result set as CSV-String with field names in first line
+     */
     public function process(Admin2_Controller_Result $result)
     {
         $result->addResponseHeader('Content-Type', 'text/plain', true);
