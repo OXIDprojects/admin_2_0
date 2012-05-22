ffc8a418d676e5d00a3e2e8a96e22b4b5fab40ba
Joscha Krug <krug@marmalade.de>
2012-05-22 10:53:11 +0200

    Restructured the frontend data

--- admin2/views/snippets/css.php
+++
@@ -1,9 +0,0 @@
-<!-- oxid admin basis -->
-<link rel="stylesheet" type="text/css" media="screen" href="css/ox_reset.css">
-<link rel="stylesheet" type="text/css" media="screen" href="css/ox_style.css">
-
-<!-- jquery ui Theme-->
-<link rel="stylesheet" type="text/css" media="screen" href="css/smoothness/jquery-ui-1.8.18.custom.css">
-
-<!-- Fluid CSS -->
-<link rel="stylesheet" type="text/css" media="screen" href="css/fluid_grid.css">
--- admin2/views/snippets/jsIncludesMain.php
+++
@@ -1,4 +0,0 @@
-<!-- <script src="js/modernizr.custom.js"></script> SCRIPT FEHLT NOCH -->
-<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
-<script src="js/jquery-ui-1.8.18.custom.min.js" type="text/javascript"></script>
-<script src="js/joscha.js" type="text/javascript"></script>
