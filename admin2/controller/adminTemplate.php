cfe45a8e4cc489320cec7a4c7ca843df9b5d5e8a
DSB <admin@mysqldumper.de>
2012-06-12 21:41:40 +0200

    QA

--- admin2/controller/adminTemplate.php
+++ admin2/controller/adminTemplate.php
@@ -8,14 +8,11 @@
  *  @link      http://admin20.de
  *  @copyright (C) 2012 :: Admin 2.0 Developers
  */
-
-
 /**
  * Controller for Creating Site
  *
  * @author Dennis Heidtmann
  * @author Rafael Dabrowski
- * @author Daniel Schlichtholz <admin@mysqldumper.de>
  * @author Joscha Krug <krug@marmalade.de>
  */
 class adminTemplate
@@ -27,41 +24,34 @@ class adminTemplate
      */
     public function run()
     {
-
         $controllerName = $this->_getClassName();
-
-        //ToDo: implement oxNew()
         $controller = new $controllerName;
-
         $template = $controller->render();
-
         $page = $this->output( $template );
-
         $this->finalize( $page );
-
     }
 
     /**
      * Function to render the Template
      *
+     * @param string $template Template name
+     *
      * @return string with the fully rendered page
      */
-    public function output( $template) {
+    public function output($template) {
 
         ob_start();
-
         require getShopBasePath() . 'views/' . $template;
-
         $page = ob_get_contents();
-
         ob_end_clean();
-
         return $page;
     }
 
     /**
      * Finalize the output end do the echo
      * prepared for the copyright output
+     *
+     * @param string $page Page content as string
      *
      * @return void
      */
@@ -70,6 +60,11 @@ class adminTemplate
         echo $page;
     }
 
+    /**
+     * Extract called class from request pram
+     *
+     * @return string
+     */
     protected function _getClassName()
     {
         //ToDo: Please implement the OXID getConfig() function
