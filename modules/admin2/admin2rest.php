100f94726494ec48753d6196c5b1224f59b92c82
Steve Knornschild <knornschild@sitzdesign.de>
2012-11-27 14:05:01 +0100

    Compability with Oxid 4.7

---
+++ modules/admin2/admin2rest.php
@@ -0,0 +1,38 @@
+<?php
+class admin2rest extends admin2rest_parent
+{
+    protected $_sThisTemplate = "admin2rest.tpl";
+    
+    public function render()
+    {
+        parent::render();
+        $soxId = $this->getEditObjectId();
+        $user = oxNew( "oxuser" );
+        $user->load( $soxId );
+        $this->_aViewData['edit'] = $user;
+        
+        return $this->_sThisTemplate;
+    }
+
+    public function save()
+    {
+        parent::save();
+
+        $soxId = $this->getEditObjectId();
+
+        if ( !$this->_allowAdminEdit( $soxId ) )
+            return false;
+
+        $aParams       = oxConfig::getParameter( "editval" );
+
+        $oUser = oxNew( "oxuser" );
+        
+        $oUser->load( $soxId );
+
+        $oUser->assign($aParams);
+        $oUser->save();
+
+        // set oxid if inserted
+        $this->setEditObjectId($oUser->getId());
+    }
+}
